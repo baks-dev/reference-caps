d3f8810bb637fd190459ed2300b4ad6b34ad50de
kingstar <admin@baks.dev>
2024-09-16 01:09:43 +0300

    Baks Development

--- BaksDevReferenceCapsBundle.php
+++ BaksDevReferenceCapsBundle.php
@@ -23,38 +23,38 @@ class BaksDevReferenceCapsBundle extends AbstractBundle
 
     public const PATH = __DIR__.DIRECTORY_SEPARATOR;
 
-    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
-    {
-        $services = $container->services()
-            ->defaults()
-            ->autowire()
-            ->autoconfigure();
-
-
-        $services->load(
-            self::NAMESPACE.'Form\\',
-            self::PATH.'Form'
-        );
-
-        $services->load(
-            self::NAMESPACE.'Type\Sizes\\',
-            self::PATH.'Type/Sizes'
-        );
-
-        $services->load(
-            self::NAMESPACE.'Listeners\\',
-            self::PATH.'Listeners'
-        );
-
-        $services->load(
-            self::NAMESPACE.'Twig\\',
-            self::PATH.'Twig'
-        );
-
-        $services->set(ReferenceChoiceSizeCaps::class)
-            ->tag('baks.reference.choice')
-            ->tag('baks.fields.choice');
-
-    }
+//    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    //    {
+    //        $services = $container->services()
+    //            ->defaults()
+    //            ->autowire()
+    //            ->autoconfigure();
+    //
+    //
+    //        $services->load(
+    //            self::NAMESPACE.'Form\\',
+    //            self::PATH.'Form'
+    //        );
+    //
+    //        $services->load(
+    //            self::NAMESPACE.'Type\Sizes\\',
+    //            self::PATH.'Type/Sizes'
+    //        );
+    //
+    //        $services->load(
+    //            self::NAMESPACE.'Listeners\\',
+    //            self::PATH.'Listeners'
+    //        );
+    //
+    //        $services->load(
+    //            self::NAMESPACE.'Twig\\',
+    //            self::PATH.'Twig'
+    //        );
+    //
+    //        $services->set(ReferenceChoiceSizeCaps::class)
+    //            ->tag('baks.reference.choice')
+    //            ->tag('baks.fields.choice');
+    //
+    //    }
 
 }
